1cea6dcf34b5d94620da0d1118fdd140a07ad564
orkhanshirin <orkhan.shirinow@gmail.com>
2022-09-09 19:43:32 +0400

    Added new template for detailed blog Defined corresponding views and urls

--- portfolio/blog/urls.py
+++ portfolio/blog/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
-    path("", views.all_blogs, name="all_blogs")
+    path("", views.all_blogs, name="all_blogs"),
+    path("<int:blog_id>/", views.detail, name="detail")
 ]
